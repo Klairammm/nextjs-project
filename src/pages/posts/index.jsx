bac258478b722a23569201c8b3605e1a615984e5
Klairammm <mclarettaf@gmail.com>
2023-02-01 18:08:57 +0100

    fix scss Posts page

--- src/pages/posts/index.jsx
+++ src/pages/posts/index.jsx
@@ -1,24 +1,30 @@
 import style from "../../styles/stylePosts.module.scss";
 
-export default function Posts({ users }) {
-  console.log(users);
+export default function Posts({ posts }) {
+  console.log(posts);
   return (
     <div className={style.Posts}>
       <h1 className={style.title}>Explore Posts</h1>
-      <div className={style.users}>
-        {users && users.map((user) => <h4 key={user.id}>{user.email}</h4>)}
+      <div className={style.posts}>
+        {posts &&
+          posts.map((post) => (
+            <div className={style.postCard}>
+              <h3 key={post.id}>{post.title}</h3>
+              <p>{post.body}</p>
+            </div>
+          ))}
       </div>
     </div>
   );
 }
 
 export async function getStaticProps() {
-  const resUsers = await fetch("https://dummyjson.com/users");
-  const dataUsers = await resUsers.json();
+  const resPosts = await fetch("https://dummyjson.com/posts");
+  const dataPosts = await resPosts.json();
 
   return {
     props: {
-      users: dataUsers.users,
+      posts: dataPosts.posts,
     },
   };
 }
